0c93a9c5bd37b78c37b0d7879a88d2d6cd963c40
cruzguzman99 <lcruz.eve@gmail.com>
2022-06-20 10:36:39 -0600

    enabling CORS

--- Pizzeria.Ports.API/Startup.cs
+++ Pizzeria.Ports.API/Startup.cs
@@ -28,13 +28,20 @@ namespace Pizzeria.Ports.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(option =>
+            {
+                option.WithOrigins("http://localhost:5000");
+                option.AllowAnyHeader();
+                option.AllowAnyMethod();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
